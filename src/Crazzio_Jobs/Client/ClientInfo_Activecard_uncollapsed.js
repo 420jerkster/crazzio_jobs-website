319629bea1c0338649029655e25c8dea0849f925
DeaDKilleR2210 <2210tatpar@gmail.com>
2021-09-11 12:18:43 +0530

    first commit

---
+++ src/Crazzio_Jobs/Client/ClientInfo_Activecard_uncollapsed.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../../public/build/tailwind.css"
+
+class ClientInfo extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ClientInfo
